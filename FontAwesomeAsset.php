e6dc98a2566590ed56031c08163feca2f102e7c1
Anton <toh1napocht@yandex.ru>
2015-10-24 22:59:14 +0300

    update font-awesome AssetBundle

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -6,9 +6,9 @@ use yii\web\AssetBundle;
 
 class FontAwesomeAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/font-awesome';
+    public $sourcePath = '@vendor/fortawesome/font-awesome';
 
     public $css = [
-        'css/font-awesome.min.css',
+        'css/font-awesome.css',
     ];
 }
